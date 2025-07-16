248e74b7415c95beb102a34e2034893eddebbaee
Werner Goebl <goebl@mdw.ac.at>
2025-07-16 13:58:07 +0200

    Expanded MEI versions added

---
+++ expand.sh
@@ -0,0 +1,3 @@
+verovio --expand 'Full-Version_Krauss1941' -a -t mei -o Full_Version_Krauss1941.mei Eljen-a-Magyar_Cranz.mei
+verovio --expand 'Full-Version_Krauss1941' -a -t timemap -o Full_Version_Krauss1941-timemap.json Eljen-a-Magyar_Cranz.mei
+verovio --expand 'Short-Version_Boskovsky1978' -a -t mei -o Short-Version_Boskovsky1978.mei Eljen-a-Magyar_Cranz.mei
